35eeb5d0bce7baa0ea5fa1cf23996fb771b534f5
olechkakulic <olga.loginova@metalab.ifmo.ru>
2022-05-11 06:41:23 +0300

    commit 1

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import pandas as pd
 import time
 import streamlit as st
 import plotly.express as px
+import streamlit as st
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from urllib.request import urlopen
+import json
+
 @st.cache
 def load_dataset(data_link):
     dataset = pd.read_csv(data_link)
@@ -13,15 +20,9 @@ titanic_data = load_dataset(titanic_link)
 #ЗАГОЛОВКИ #
 st.title("Калькулятор Брэгговского зеркала")
 st.markdown("Welcome to this in-depth introduction to [...].")
-st.header("Customary quote")
-st.markdown("> I just love to go home, no matter where I am [...]")
-#ПРИКОЛЬНАЯ ШТУКА #
-age_columns = st.beta_columns(2)
-age_min = age_columns[0].number_input("Minimum Age", value=titanic_data['age'].min())
-age_max = age_columns[1].number_input("Maximum Age", value=titanic_data['age'].max())
-if age_max < age_min:
-    st.error("The maximum age can't be smaller than the minimum age!")
-else:
-    st.success("Congratulations! Correct Parameters!")
-    subset_age = titanic_data[(titanic_data['age'] <= age_max) & (age_min <= titanic_data['age'])]
-    st.write(f"Number of Records With Age Between {age_min} and {age_max}: {subset_age.shape[0]}")
+#код для вводимых данных
+st.number_input('Длина волны', min_value = 0, max_value = 1000, step = 1)
+st.number_input('Значение угла', min_value = 0, max_value = 180, step = 1)
+st.selectbox(
+     'Тип поляризации',
+     ('TE', 'TM',))
