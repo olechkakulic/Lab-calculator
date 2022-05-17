83ac563e05d8bdc9a873ae82d67d54c4c8ff797a
olechkakulic <olga.loginova@metalab.ifmo.ru>
2022-05-18 02:58:22 +0300

    eto pi

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ def get_step(material_name, type):
         return float(globals()[material_name]['wl'].values[0])
     elif type==2:
         return float(globals()[material_name]['wl'].values[-1])
+
+def get_coefficent(mat, inp_wl):
+    wl_list = globals()[mat]['wl'].tolist()
+    nearest_wl = min(wl_list, key=lambda x: abs(x - inp_wl))
+    number = wl_list.index(nearest_wl)
+    return float(globals()[mat]['n'].values[number])
+
+#def get_koef(koefficient):
+        #return float(globals()[koefficient]['n'].values[])
 @st.cache
 def load_dataset(data_link):
     dataset = pd.read_csv(data_link)
@@ -39,7 +48,11 @@ material=st.sidebar.selectbox(
      'Материал',
      (filenames))
 if material:
-    st.sidebar.number_input('Длина волны ', min_value=get_step(str(material), 1), max_value=get_step(str(material), 2))
+    input_wl=st.sidebar.number_input('Длина волны ', min_value=get_step(str(material), 1), max_value=get_step(str(material), 2))
+# st.sidebar.number_input('n', value = get_koef)
+    if input_wl:
+        st.sidebar.write('n', get_coefficent(str(material),float(input_wl)))
+
 
 
 
