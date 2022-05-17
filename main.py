b0f23e4f656bd2e4571b5d085bddf31e55a4a79d
olechkakulic <olga.loginova@metalab.ifmo.ru>
2022-05-17 22:21:20 +0300

    lol

--- main.py
+++ main.py
@@ -11,11 +11,23 @@ import json
 import os
 
 from openpyxl import load_workbook
+
+def get_step(material_name, type):
+    if type==1:
+        return int(globals()[material_name].at[0, "wl"])
+    elif type==2:
+        return int(globals()[material_name].at[-1, "wl"])
 @st.cache
 def load_dataset(data_link):
     dataset = pd.read_csv(data_link)
     return dataset
 
+directory = 'DataFiles'
+files = os.listdir(directory)
+filenames=[]
+for i in range(len(files)):
+        globals()[(files[i][:-4])] = pd.read_csv('DataFiles/'+files[i])
+        filenames.append(str(files[i][:-4]))
 
 titanic_link = 'https://raw.githubusercontent.com/mwaskom/seaborn-data/master/titanic.csv'
 titanic_data = load_dataset(titanic_link)
@@ -23,33 +35,28 @@ titanic_data = load_dataset(titanic_link)
 st.title("Калькулятор Брэгговского зеркала")
 st.markdown("Люблю Дашеньку Салтыкову")
 #код для вводимых данных
-st.sidebar.number_input('Длина волны', min_value = 0, max_value = 1000, step = 1)
 st.sidebar.number_input('Значение угла', min_value = 0, max_value = 180, step = 1)
 st.sidebar.selectbox(
      'Тип поляризации',
      ('TE', 'TM',))
-material = ['Ag',"Au"]
-st.sidebar.selectbox(
+material=st.sidebar.selectbox(
      'Материал',
-     (material))
+     (filenames))
 
+if material:
+    st.sidebar.number_input('Длина волны ', min_value=get_step(str(material), 1), max_value=get_step(str(material), 2), step=1)
 
-# прога ептить пробная попытка запустить файл нахуй.
-wb = load_workbook(r"C:\Users\4814285\lab culc\Johnson.xlsx")
-sheet = wb.get_sheet_by_name('Worksheet')
-# норм попытка для Ag
-wb = load_workbook("D:\Ag.xlsx")
-sheet = wb.get_sheet_by_name('Worksheet')
+# # прога ептить пробная попытка запустить файл нахуй.
+# sheet = wb.get_sheet_by_name('Worksheet')
+# # норм попытка для Ag
+# wb = load_workbook("D:\Ag.xlsx")
+# sheet = wb.get_sheet_by_name('Worksheet')
 #b - Длина волны из столбцов
 #def blocks(material,b)
     #if b != sheet.cell(column=1).value:
 #return **
 # работа с папкой, в которой лежат файлы
-directory = 'D:\материалы'
-files = os.listdir(directory)
-for i in range (len(files)):
-    input(files[i])
-    os.path.splitext(D:\материалы)[0]
+
 
 
 
