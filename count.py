fb5816f77033a33e0f979dd8552f7aae23784603
BuildTools <unconfigured@null.spigotmc.org>
2022-06-09 01:34:05 +0300

    не работает

--- count.py
+++ count.py
@@ -1,85 +1,85 @@
 import matplotlib.pyplot as plt
 import numpy
 from numpy import pi, sqrt, cos, arange, sin, exp, array, absolute, real  #
-def calc_TE_pol(n, nl, angle, h, wl, n_pod, n_air):
-    global T01
-    #polarisation = ['TE', 'TM']
-    def get_T_prev(n, n_pod, angle, n_prev, n_next):
-        kx = n_pod * sin(angle)
 
+
+def calc_TE_pol(n, nl, angle, h, wl, n_pod, n_air, polarisation):
+    global T01
+
+    def get_T_prev(n, n_pod, angle, n_prev, n_next):
+        global r_prev, t_prev, t, r
+        kx = n_pod * sin(angle)
 
         def kzi(ni):
             kzi = numpy.sqrt(ni ** 2 - kx ** 2)
             return kzi
-        #if polarisation[1]:
-        r_prev = (kzi(n_prev) - kzi(n))/(kzi(n_prev) + kzi(n))
-                #r_prev = r(i-1,i-1)
-        r = (kzi(n) - kzi(n_next))/(kzi(n) + kzi(n_next))
-                #r = r(i,i)
-        t = 2*kzi(n_prev)/(kzi(n_prev) + kzi(n))
-                #t = t(i,i-1)
-        t_prev = 2*kzi(n)/(kzi(n) + kzi(n_next))
-                #t_prev = t(i-1,i)
-        #if polarisation[2]:
-                # #TM POLARISATION
-                #def Ei(ni):
-                #     Ei = ni ^ 2
-                #     return Ei
-                #r_prev = (Ei(n)*kzi(n_prev) - Ei(n_prev)*kzi(n))/(Ei(n)*kzi(n_prev) + Ei(n_prev)*kzi(n))
-                #r = (Ei(n_next)*kzi(n) - Ei(n)*kzi(n_next)) /Ei(n_next)(kzi(n) +Ei(n)*kzi(n_next))
-                #t = 2 * Ei(n)*kzi(n_prev) /Ei(n)*(kzi(n_prev) +Ei(n_prev)*kzi(n))
-                #t_prev = 2*Ei(n_next) * kzi(n) /(Ei(n_next)*kzi(n) +Ei(n)*kzi(n_next))
 
-        Tprev=[[0,0],[0,0]]
-        Tprev[0][0]=(t_prev*t-r*r_prev)/(t_prev)
-        Tprev[0][1]=r/(t_prev)
-        Tprev[1][0]=(-1)*r_prev/(t_prev)
-        Tprev[1][1]=1/(t_prev)
+        if polarisation == 'TE':
+            r_prev = (kzi(n_prev) - kzi(n)) / (kzi(n_prev) + kzi(n))
+            r = (kzi(n) - kzi(n_next)) / (kzi(n) + kzi(n_next))
+            t = 2 * kzi(n_prev) / (kzi(n_prev) + kzi(n))
+            t_prev = 2 * kzi(n) / (kzi(n) + kzi(n_next))
+        elif polarisation == 'TM':
+            # #TM POLARISATION
+            def Ei(ni):
+                Ei = ni ** 2
+                return Ei
+
+            r_prev = (Ei(n) * kzi(n_prev) - Ei(n_prev) * kzi(n)) / (Ei(n) * kzi(n_prev) + Ei(n_prev) * kzi(n))
+            r = (Ei(n_next) * kzi(n) - Ei(n) * kzi(n_next)) / (Ei(n_next) * kzi(n) + Ei(n) * kzi(n_next))
+            t = 2 * Ei(n) * kzi(n_prev) / Ei(n) * (kzi(n_prev) + Ei(n_prev) * kzi(n))
+            t_prev = 2 * Ei(n_next) * kzi(n) / (Ei(n_next) * kzi(n) + Ei(n) * kzi(n_next))
+
+        Tprev = [[0, 0], [0, 0]]
+        Tprev[0][0] = (t_prev * t - r * r_prev) / t_prev
+        Tprev[0][1] = r / t_prev
+        Tprev[1][0] = (-1) * r_prev / t_prev
+        Tprev[1][1] = 1 / t_prev
         return array(Tprev)
 
-
-    def get_T(h, n_pod, angle,n, lamb):
+    def get_T(h, n_pod, angle, n, lamb):
         kx = n_pod * sin(angle)
 
         # должно быть n_0 sin angle, angle = sup
         def kzi(ni):
             kzi = numpy.sqrt(ni ** 2 - kx ** 2)
             return kzi
-    #lamb - длина волны
-        T=[[0, 0], [0, 0]]
-        T[0][0]=exp((2*pi*(0+1j)*kzi(n)*h*10**(-6))/(lamb*10**(-6)))
-        T[0][1]=0
-        T[1][0]=0
-        T[1][1]=exp((-1)*2*pi*(0+1j)*kzi(n)*h*10**(-6)/(lamb*10**(-6)))
+
+        # lamb - длина волны
+        T = [[0, 0], [0, 0]]
+        T[0][0] = exp((2 * pi * (0 + 1j) * kzi(n) * h * 10 ** (-6)) / (lamb * 10 ** (-6)))
+        T[0][1] = 0
+        T[1][0] = 0
+        T[1][1] = exp((-1) * 2 * pi * (0 + 1j) * kzi(n) * h * 10 ** (-6) / (lamb * 10 ** (-6)))
         return array(T)
 
-    if n==0:
-        T01=get_T_prev(nl[0], n_pod, angle, n_pod, n_air)
-        T01=T01.dot(get_T(h[0],n_pod,angle,nl[0],wl))
-        T01=T01.dot(get_T_prev(n_air,n_pod,angle,nl[0],n_air))
+    if n == 0:
+        T01 = get_T_prev(nl[0], n_pod, angle, n_pod, n_air)
+        T01 = T01.dot(get_T(h[0], n_pod, angle, nl[0], wl))
+        T01 = T01.dot(get_T_prev(n_air, n_pod, angle, nl[0], n_air))
     else:
-        T01=get_T_prev(nl[0],n_pod,angle,n_pod,nl[1])
-        T01=T01.dot(get_T(h[0],n_pod,angle,nl[0],wl))
-        for i in range (1, n):
+        T01 = get_T_prev(nl[0], n_pod, angle, n_pod, nl[1])
+        T01 = T01.dot(get_T(h[0], n_pod, angle, nl[0], wl))
+        for i in range(1, n):
             T01 = T01.dot(get_T_prev(nl[i], n_pod, angle, nl[i - 1], nl[i + 1]))
-            T01 = T01.dot(get_T(h[i],n_pod,angle,nl[i],wl))
+            T01 = T01.dot(get_T(h[i], n_pod, angle, nl[i], wl))
         T01 = T01.dot(get_T_prev(nl[-1], n_pod, angle, nl[-2], n_air))
-    tau0N=1/T01[1][1] #луч идет сверху
-    r00=-T01[1][0]/T01[1][1]
-    rnn=T01[0][1]/T01[1][1]
-    tauN_0=T01[0][0]+(r00*rnn)/tau0N # луч идет снизу
-    #POWER REFLECTION
-    R00=absolute(r00)**2
-    RNN=absolute(rnn)**2
-    #POWER TRANSMISSION FOR TE:
-    kz0=numpy.sqrt(n_air**2 - (n_pod*sin(angle))**2)
-    kzN1=numpy.sqrt(n_pod**2 - (n_pod*sin(angle))**2)
-    #if polarisation[1]:
-    TBIG_0N=(absolute(tau0N)**2)*real(kz0/kzN1)
-    TBIG_N0=(absolute(tauN_0)**2)*real(kzN1/kz0)
-    #if polarisation[2]:
-        #E_0 = n_air**2
-        #E_N = n_pod**2
-        #TBIG_0N=(absolute(tau0N)**2)*real((kz0*E_N)/(kzN1*E_0))
-        #TBIG_N0=(absolute(tauN_0)**2)*real((kzN1*E_0)/(kz0*E_N))
+    tau0N = 1 / T01[1][1]  # луч идет сверху
+    r00 = -T01[1][0] / T01[1][1]
+    rnn = T01[0][1] / T01[1][1]
+    tauN_0 = T01[0][0] + (r00 * rnn) / tau0N  # луч идет снизу
+    # POWER REFLECTION
+    R00 = absolute(r00) ** 2
+    RNN = absolute(rnn) ** 2
+    # POWER TRANSMISSION FOR TE:
+    kz0 = numpy.sqrt(n_air ** 2 - (n_pod * sin(angle)) ** 2)
+    kzN1 = numpy.sqrt(n_pod ** 2 - (n_pod * sin(angle)) ** 2)
+    if polarisation == 'TE':
+        TBIG_0N = (absolute(tau0N) ** 2) * real(kz0 / kzN1)
+        TBIG_N0 = (absolute(tauN_0) ** 2) * real(kzN1 / kz0)
+    elif polarisation == 'TM':
+        E_0 = n_air ** 2
+        E_N = n_pod ** 2
+        TBIG_0N = (absolute(tau0N) ** 2) * real((kz0 * E_N) / (kzN1 * E_0))
+        TBIG_N0 = (absolute(tauN_0) ** 2) * real((kzN1 * E_0) / (kz0 * E_N))
     return R00, RNN, TBIG_0N, TBIG_N0
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-#import time
+# import time
 import numpy
 import streamlit as st
 import pandas as pd
 from numpy import pi
 import numpy as np
 from count import *
-#from urllib.request import urlopen
-#import json
+# from urllib.request import urlopen
+# import json
 import os
-#from openpyxl import load_workbook
+# from openpyxl import load_workbook
 from count import calc_TE_pol
 from bokeh.plotting import figure
+
 
 # def get_step(material_name, types):
 #     if types == 1:
@@ -19,74 +20,81 @@ from bokeh.plotting import figure
 #         return float(globals()[material_name]['wl'].values[-1])
 
 def get_coefficent(mat, inp_wl):
-    wl_list = globals()[mat]['wl'].tolist()# работаем с переменной global добавляем столбец wl в список
-    nearest_wl = min(wl_list, key=lambda x: abs(x - inp_wl))# берем ближайшее к вводимой длине, по значениям которой сортируются элементы массива.
-    number = wl_list.index(nearest_wl)# присваиваем индекс от ближ волны переменной wl list
-    return float(globals()[mat]['n'].values[number])# выводим значение n ки для этого номера
+    wl_list = globals()[mat]['wl'].tolist()  # работаем с переменной global добавляем столбец wl в список
+    nearest_wl = min(wl_list, key=lambda x: abs(
+        x - inp_wl))  # берем ближайшее к вводимой длине, по значениям которой сортируются элементы массива.
+    number = wl_list.index(nearest_wl)  # присваиваем индекс от ближ волны переменной wl list
+    return float(globals()[mat]['n'].values[number])  # выводим значение n ки для этого номера
 
-def update_n(a,inp_wl):
-    n=[]
+
+def update_n(a, inp_wl):
+    n = []
     for mat in a:
         n.append(get_coefficent(str(mat), float(inp_wl)))
     return n
+
+
 # работа с файлами
 @st.cache
 def load_dataset(data_link):
     dataset = pd.read_csv(data_link)
     return dataset
+
+
 directory = 'DataFiles'
 files = os.listdir(directory)
-filenames=[]
+filenames = []
 for i in range(len(files)):
-        globals()[(files[i][:-4])] = pd.read_csv('DataFiles/'+files[i])
-        filenames.append(str(files[i][:-4]))
+    globals()[(files[i][:-4])] = pd.read_csv('DataFiles/' + files[i])
+    filenames.append(str(files[i][:-4]))
 
-#ЗАГОЛОВКИ
+# ЗАГОЛОВКИ
 st.title("Калькулятор Брэгговского зеркала")
 st.markdown("Люблю Дашеньку Салтыкову")
-#код для вводимых данных
-angle_input =st.sidebar.number_input('Значение угла', min_value=0, max_value=90, step=1)
-angle_input=angle_input*2*pi/360
+# код для вводимых данных
+angle_input = st.sidebar.number_input('Значение угла', min_value=0, max_value=90, step=1)
+angle_input = angle_input * 2 * pi / 360
 polarisation = st.sidebar.selectbox(
-     'Тип поляризации',
-     ('TE', 'TM',))
-n_pod=1.00
-n_air =st.sidebar.number_input('Коэф-т преломления подложки', min_value=1.00, max_value=4.00, step=0.1)
+    'Тип поляризации',
+    ('TE', 'TM',))
+n_pod = 1.00
+n_air = st.sidebar.number_input('Коэф-т преломления подложки', min_value=1.00, max_value=4.00, step=0.1)
 input_wl = st.sidebar.number_input(f"Длина волны,мкм", min_value=0.4, max_value=1.5, step=0.05)
 container = st.container()
-ncol = st.sidebar.number_input("Введите количество слоев",  min_value=0, step=1)
-nal=[]
-h=[]
-layers_name=[]
+ncol = st.sidebar.number_input("Введите количество слоев", min_value=0, step=1)
+nal = []
+h = []
+layers_name = []
 
 for i in range(ncol):
-    a = st.sidebar.selectbox(f"Номер слоя # {i+1}", (filenames), key=i)
+    a = st.sidebar.selectbox(f"Номер слоя # {i + 1}", (filenames), key=i)
     layers_name.append(a)
     if a:
-        tolshina = st.sidebar.number_input(f"Толщина,нм {i+1} слоя", min_value=0, max_value=800, step=10)
-        h.append(tolshina*10**(-9))
-        enable_wave_lenght = st.sidebar.checkbox(f'Коэф-т приломления слоя {i+1} постоянен')
+        tolshina = st.sidebar.number_input(f"Толщина,нм {i + 1} слоя", min_value=0, max_value=800, step=10)
+        h.append(tolshina * 10 ** (-9))
+        enable_wave_lenght = st.sidebar.checkbox(f'Коэф-т приломления слоя {i + 1} постоянен')
         if enable_wave_lenght:
-            n = st.sidebar.number_input(f'Введите коэффициент преломления {i+1} слоя', min_value=0.001, max_value=5.00, step=0.01)
+            n = st.sidebar.number_input(f'Введите коэффициент преломления {i + 1} слоя', min_value=0.001,
+                                        max_value=5.00, step=0.01)
             nal.append(n)
         else:
             na = get_coefficent(str(a), float(input_wl))
-            n = st.sidebar.write(f' n({i+1}) = {na}')
+            n = st.sidebar.write(f' n({i + 1}) = {na}')
             nal.append(na)
-if st.button('хочу убить себя') and polarisation=='TE' and ncol>0:
-    BOba = calc_TE_pol(ncol-1, nal, angle_input, h, input_wl, n_pod, n_air)
+if st.button('хочу убить себя') and ncol > 0:
+    BOba = calc_TE_pol(ncol - 1, nal, angle_input, h, input_wl, n_pod, n_air, str(polarisation))
     st.write(BOba)
-    angles=[]
-    #powers00=[]
-    powersnn=[]
-    #T_0N=[]
-    T_N0_angle=[]
-    for i in range(0,90,1):
-        i=i*2*pi/360
+    angles = []
+    # powers00=[]
+    powersnn = []
+    # T_0N=[]
+    T_N0_angle = []
+    for i in range(0, 90, 1):
+        i = i * 2 * pi / 360
         angles.append(i)
-        #powers00.append(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air)[0])
-        powersnn.append(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air)[1])
-        T_N0_angle.append(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air)[3])
+        # powers00.append(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air)[0])
+        powersnn.append(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air, str(polarisation))[1])
+        T_N0_angle.append(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air, str(polarisation))[3])
     # fig1=figure(
     #     title='R00(angle)',
     #     x_axis_label='angle, grad',
@@ -111,17 +119,18 @@ if st.button('хочу убить себя') and polarisation=='TE' and ncol>0:
     fig3.line(angles, T_N0_angle, legend_label='Olya22', line_width=2)
     st.bokeh_chart(fig3, use_container_width=True)
 
-    powersnn_wave=[]
-    T_N0_wave=[]
-    wawes=[]
+    powersnn_wave = []
+    T_N0_wave = []
+    wawes = []
     for waw in numpy.arange(0.4, 0.8, 0.05):
         wawes.append(waw)
-        p=calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air)[1]
+        p = calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air, str(polarisation))[1]
         powersnn_wave.append(p)
         print(p)
-        T_N0_wave.append(calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air)[3])
+        T_N0_wave.append(
+            calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air, str(polarisation))[3])
 
-    st.write(powersnn_wave,T_N0_wave)
+    st.write(powersnn_wave, T_N0_wave)
     fig3 = figure(
         title='RNN(wave)',
         x_axis_label='wave, нм',
