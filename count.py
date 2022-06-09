2429e5e6a95effb6cce38d0f7bece4716fe15efc
olechkakulic <olga.loginova@metalab.ifmo.ru>
2022-06-10 02:25:01 +0300

    окончательная версия

--- count.py
+++ count.py
@@ -13,22 +13,23 @@ def calc_TE_pol(n, nl, angle, h, wl, n_pod, n_air, polarisation):
         def kzi(ni):
             kzi = numpy.sqrt(ni ** 2 - kx ** 2)
             return kzi
-
         if polarisation == 'TE':
             r_prev = (kzi(n_prev) - kzi(n)) / (kzi(n_prev) + kzi(n))
             r = (kzi(n) - kzi(n_next)) / (kzi(n) + kzi(n_next))
             t = 2 * kzi(n_prev) / (kzi(n_prev) + kzi(n))
             t_prev = 2 * kzi(n) / (kzi(n) + kzi(n_next))
         elif polarisation == 'TM':
-            # #TM POLARISATION
             def Ei(ni):
                 Ei = ni ** 2
                 return Ei
-
-            r_prev = (Ei(n) * kzi(n_prev) - Ei(n_prev) * kzi(n)) / (Ei(n) * kzi(n_prev) + Ei(n_prev) * kzi(n))
-            r = (Ei(n_next) * kzi(n) - Ei(n) * kzi(n_next)) / (Ei(n_next) * kzi(n) + Ei(n) * kzi(n_next))
-            t = 2 * Ei(n) * kzi(n_prev) /( Ei(n) * kzi(n_prev) + Ei(n_prev) * kzi(n))
-            t_prev = 2 * Ei(n_next) * kzi(n) / (Ei(n_next) * kzi(n) + Ei(n) * kzi(n_next))
+            r_prev = (Ei(n) * kzi(n_prev) - Ei(n_prev) * kzi(n)) /\
+                     (Ei(n) * kzi(n_prev) + Ei(n_prev) * kzi(n))
+            r = (Ei(n_next) * kzi(n) - Ei(n) * kzi(n_next)) /\
+                (Ei(n_next) * kzi(n) + Ei(n) * kzi(n_next))
+            t = 2 * Ei(n) * kzi(n_prev) /\
+                ( Ei(n) * kzi(n_prev) + Ei(n_prev) * kzi(n))
+            t_prev = 2 * Ei(n_next) * kzi(n) / \
+                     (Ei(n_next) * kzi(n) + Ei(n) * kzi(n_next))
 
         Tprev = [[0, 0], [0, 0]]
         Tprev[0][0] = (t_prev * t - r * r_prev) / t_prev
@@ -39,13 +40,9 @@ def calc_TE_pol(n, nl, angle, h, wl, n_pod, n_air, polarisation):
 
     def get_T(h, n_pod, angle, n, lamb):
         kx = n_pod * sin(angle)
-
-        # должно быть n_0 sin angle, angle = sup
         def kzi(ni):
             kzi = numpy.sqrt(ni ** 2 - kx ** 2)
             return kzi
-
-        # lamb - длина волны
         T = [[0, 0], [0, 0]]
         T[0][0] = exp((2 * pi * (0 + 1j) * kzi(n) * h * 10 ** (-6)) / (lamb * 10 ** (-6)))
         T[0][1] = 0
@@ -64,14 +61,10 @@ def calc_TE_pol(n, nl, angle, h, wl, n_pod, n_air, polarisation):
             T01 = T01.dot(get_T_prev(nl[i], n_pod, angle, nl[i - 1], nl[i + 1]))
             T01 = T01.dot(get_T(h[i], n_pod, angle, nl[i], wl))
         T01 = T01.dot(get_T_prev(nl[-1], n_pod, angle, nl[-2], n_air))
-    tau0N = 1 / T01[1][1]  # луч идет сверху
+    tau0N = 1 / T01[1][1]
     r00 = -T01[1][0] / T01[1][1]
     rnn = T01[0][1] / T01[1][1]
-    tauN_0 = T01[0][0] + (r00 * rnn) / tau0N  # луч идет снизу
-    # POWER REFLECTION
-    #R00 = absolute(r00) ** 2
-    #RNN = absolute(rnn) ** 2
-    # POWER TRANSMISSION FOR TE:
+    tauN_0 = T01[0][0] + (r00 * rnn) / tau0N
     kz0 = numpy.sqrt(n_air ** 2 - (n_pod * sin(angle)) ** 2)
     kzN1 = numpy.sqrt(n_pod ** 2 - (n_pod * sin(angle)) ** 2)
     if polarisation == 'TE':
--- main.py
+++ main.py
@@ -14,29 +14,22 @@ def interpol(mat, wl):
                 return (line_cur[1] - line_prev[1]) / (line_cur[0] - line_prev[0]) * (
                             wl - line_prev[0]) + line_prev[1]
     return 1
-
 def update_n(a, inp_wl):
     n = []
     for mat in a:
         n.append(interpol(str(mat), float(inp_wl)))
     return n
-
-# работа с файлами
 @st.cache
 def load_dataset(data_link):
     dataset = pd.read_csv(data_link)
     return dataset
-
-
 directory = 'DataFiles'
 files = os.listdir(directory)
 filenames = []
 for i in range(len(files)):
     filenames.append(str(files[i][:-4]))
 
-# ЗАГОЛОВКИ
 st.title("Калькулятор Брэгговского зеркала")
-# код для вводимых данных
 angle_input = st.sidebar.number_input('Значение угла', min_value=0, max_value=85, step=5)
 angle_input = angle_input * 2 * pi / 360
 polarisation = st.sidebar.selectbox(
@@ -44,7 +37,7 @@ polarisation = st.sidebar.selectbox(
     ('TE', 'TM',))
 n_pod = 1.00
 n_air = 1.00
-input_wl = st.sidebar.number_input(f"Длина волны,мкм", min_value=0.4, max_value=0.8, step=0.05)
+input_wl = st.sidebar.number_input(f"Длина волны, мкм", min_value=0.4, max_value=0.8, step=0.05)
 container = st.container()
 ncol = st.sidebar.number_input("Введите количество слоев", min_value=0, step=1)
 nal = []
@@ -55,7 +48,7 @@ for i in range(ncol):
     a = st.sidebar.selectbox(f"Материал слоя # {i + 1}", (filenames), key=i)
     layers_name.append(a)
     if a:
-        tolсhina = st.sidebar.number_input(f"Толщина {i + 1} слоя,мкм", min_value=0.03, max_value=0.4, step=0.01)
+        tolсhina = st.sidebar.number_input(f"Толщина {i + 1} слоя, мкм", min_value=0.03, max_value=0.4, step=0.01)
         h.append(tolсhina * 10 ** (-6))
         enable_wave_lenght = st.sidebar.checkbox(f'Коэф-т преломления слоя {i + 1} постоянен')
         if enable_wave_lenght:
@@ -63,26 +56,21 @@ for i in range(ncol):
                                         max_value=5.00, step=0.01)
             nal.append(n)
         else:
-            # na = get_coefficent(str(a), float(input_wl))
             na = interpol(str(a), float(input_wl))
             n = st.sidebar.write(f' n({i + 1}) = {na}')
             nal.append(na)
-            #nal.append(f"{na:.2}")
 if st.button('Рассчитать') and ncol > 0:
     r_coef1, t_coef1 = calc_TE_pol(ncol - 1, nal, angle_input, h, input_wl, n_pod, n_air, str(polarisation))
-    Result = f"Коэффициент отражения: {round(r_coef1,4)}",f" Коэффициент пропускания: {round(t_coef1,4)}"
-    st.info(Result)
+    st.info(f"Коэффициент отражения: {round(r_coef1,4)}")
+    st.info(f"Коэффициент пропускания: {round(t_coef1,4)}")
     angles = []
-    # powers00=[]
     powersnn = []
-    # T_0N=[]
     T_N0_angle = []
     for i in range(0, 90, 1):
         i = i * 2 * pi / 360
         angles.append(i)
-        # powers00.append(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air)[0])
-        powersnn.append(round(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air, str(polarisation))[0], 3))
-        T_N0_angle.append(round(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air, str(polarisation))[1], 3))
+        powersnn.append(round(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air, str(polarisation))[0], 5))
+        T_N0_angle.append(round(calc_TE_pol(ncol - 1, nal, i, h, input_wl, n_pod, n_air, str(polarisation))[1], 5))
     c1, c2 = st.columns(2)
     with c1:
         fig1 = figure(
@@ -90,7 +78,7 @@ if st.button('Рассчитать') and ncol > 0:
             height=400,
             title='Power reflection(angle)',
             x_axis_label='angle, rad',
-            y_axis_label='Power reflection'
+            y_axis_label='Reflection'
     )
         fig1.line(angles, powersnn, line_width=2)
         st.bokeh_chart(fig1, use_container_width=True)
@@ -100,7 +88,7 @@ if st.button('Рассчитать') and ncol > 0:
             height=400,
             title='Transmission coefficient(angle)',
             x_axis_label='angle, rad',
-            y_axis_label='Transmission coefficient'
+            y_axis_label='Transmission'
         )
         fig2.line(angles, T_N0_angle, line_width=2)
         st.bokeh_chart(fig2, use_container_width=True)
@@ -108,28 +96,35 @@ if st.button('Рассчитать') and ncol > 0:
     powersnn_wave = []
     T_N0_wave = []
     wawes = []
+    k = 0
     for waw in numpy.arange(0.4, 0.8, 0.001):
         wawes.append(waw)
-        p = calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air, str(polarisation))[0]
+        for i in range(0, len(nal)):
+            if nal[i] != 1:
+                k = 1
+        if k != 1:
+                p = calc_TE_pol(ncol - 1, nal, angle_input, h, waw, n_pod, n_air, str(polarisation))[
+                0]
+                s = \
+                calc_TE_pol(ncol - 1,nal, angle_input, h, waw, n_pod, n_air, str(polarisation))[
+                    1]
+
+        else:
+            p = calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air, str(polarisation))[0]
+            s = calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air, str(polarisation))[
+                1]
         if p<=0:
             powersnn_wave.append(0)
         elif p>0 and p<1:
             powersnn_wave.append(round(p, 5))
         elif p>=1:
             powersnn_wave.append(1)
-        #powersnn_wave.append(round(p, 5))
-        #print(p)
-        s=calc_TE_pol(ncol - 1, update_n(layers_name, waw), angle_input, h, waw, n_pod, n_air, str(polarisation))[1]
         if s <= 0:
             T_N0_wave.append(0)
         elif s > 0 and s < 1:
             T_N0_wave.append(round(s, 5))
         elif s >= 1:
             T_N0_wave.append(1)
-            # powersnn_wave.append(round(p, 5))
-        #print(s)
-
-    #st.write(powersnn_wave, T_N0_wave)
     c3, c4 = st.columns(2)
     with c3:
         fig3 = figure(
@@ -137,7 +132,7 @@ if st.button('Рассчитать') and ncol > 0:
             height=400,
             title='Power reflection(wave)',
             x_axis_label='wave, µm',
-            y_axis_label='Power reflection'
+            y_axis_label='Reflection'
     )
         fig3.line(wawes, powersnn_wave, line_width=2)
         st.bokeh_chart(fig3, use_container_width=True)
@@ -147,7 +142,7 @@ if st.button('Рассчитать') and ncol > 0:
             height=400,
             title='Transmission coefficient(wave)',
             x_axis_label='wave, µm',
-            y_axis_label='Transmission coefficient'
+            y_axis_label='Transmission'
     )
         fig4.line(wawes, T_N0_wave, line_width=2)
         st.bokeh_chart(fig4, use_container_width=True)
